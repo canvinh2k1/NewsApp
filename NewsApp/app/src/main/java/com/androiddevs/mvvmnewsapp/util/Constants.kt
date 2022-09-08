0874fa1ce5e7f64e8177604e93561e039fe9d2fd
canvinh2k1 <19020658@vnu.edu.vn>
2022-09-08 22:59:57 +0700

    newsapp

---
+++ NewsApp/app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "5be78e139fb748fe957381e24b22dfea"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
